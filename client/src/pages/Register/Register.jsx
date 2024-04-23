a3279d06b03a47be4be9a54a83df8b374ee221ad
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-23 09:58:50 +0200

    add the inputs in registerForm

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import  { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: {errors} } = useForm();
+
+
+
   return (
     <>
         <form className="register" onSubmit={handleSubmit(onSubmit)}>
-            <input name='name' {...register('name', {required:true})} className="register_username" placeholder="Nombre" required/>
-            <input type="email" name='email' {...register('email', {required:true})} className="register_email" placeholder="Email" required/>
-            <input type="password" name='password' {...register('password', {required:true})} className="register_password" placeholder="Contraseña" required/>
+            <input name='name' className="registerUsername" placeholder="Nombre" {...register('name', {required:true})} required/>
+            <input type="email" name='email' className="registerEmail" placeholder="Email" {...register('email', {required:true})} required/>
+            <input type="password" name='password' className="registerPassword" placeholder="Contraseña" {...register('password', {required:true})} required/>
             <button type="submit">Register</button>
             <p>¿Ya tienes una cuenta?</p>
-            <button onClick={() => navigate(`/login`)} className='registrer_option_changer'>Login</button>
+            <button  className='ChangeToLoginButton' onClick={() => navigate(`/login`)}>Login</button>
         </form>
     </>
   )
