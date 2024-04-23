5224361fb6084c5d9a91cc963f65f1fdfab5f83a
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-23 09:55:09 +0200

    add navigate and useForm to register formulary

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -3,7 +3,16 @@ import  { useForm } from 'react-hook-form'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+        <form className="register" onSubmit={handleSubmit(onSubmit)}>
+            <input name='name' {...register('name', {required:true})} className="register_username" placeholder="Nombre" required/>
+            <input type="email" name='email' {...register('email', {required:true})} className="register_email" placeholder="Email" required/>
+            <input type="password" name='password' {...register('password', {required:true})} className="register_password" placeholder="Contraseña" required/>
+            <button type="submit">Register</button>
+            <p>¿Ya tienes una cuenta?</p>
+            <button onClick={() => navigate(`/login`)} className='registrer_option_changer'>Login</button>
+        </form>
+    </>
   )
 }
 
