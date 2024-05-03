a02a948b02288e2334bfb516d6cfd9729f6d726e
EstefanyBatPel <estefanybatpel@gmail.com>
2024-05-03 12:29:27 +0200

    changes in layout import footer and card import selectAll

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./nav/Nav";
-import Footer from "./footer/Footer";
+import Footer from "./footer/footer";
 
 const LayoutPublic = () => { 
     return ( 
