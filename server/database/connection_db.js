149b8a936c9bccfaec5a0975a83f17cca3ba2c75
jessica noguera <jnoguera.fact@gmail.com>
2024-05-13 14:22:14 +0200

    cambios

--- server/database/connection_db.js
+++ server/database/connection_db.js
@@ -8,10 +8,10 @@ const connection_db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
 
-  // logging: (msg) => {
+  logging: (msg) => {
   //   // Filtrar los mensajes que comienzan con 'Executing' para mostrar solo las consultas ejecutadas
-  //   return msg.startsWith('Executing');
-  // }
+   return msg.startsWith('Executing');
+  }
 
 });
 
