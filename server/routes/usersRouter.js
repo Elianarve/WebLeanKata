d703f3ad7401c326882edaacb38b00367fae29b1
jessica noguera <jnoguera.fact@gmail.com>
2024-05-15 16:33:21 +0200

    usueradmin

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,19 +1,15 @@
 import express from 'express';
-import { getUser, deleteUser, createUser, updateUser, getOneUser } from '../controllers/usersController.js';
+import { getUser, createUser, updateUser, getOneUser } from '../controllers/usersController.js';
 import { userValidator, updateUserValidator } from '../validators/usersValidator.js';
 import handleValidationResults from '../helpers/validationHelper.js';
 import { authRol } from '../middleware/rolMiddleware.js';
 
-const router = express.Router();
+const userRouter = express.Router();
 
-router.get('/', authRol(['user']), getUser);
+userRouter.get('/', authRol(['user']), getUser);
+userRouter.post('/', authRol(['user']), userValidator, handleValidationResults, createUser);
+userRouter.put('/:id', authRol(['user']), updateUserValidator, handleValidationResults, updateUser);
+userRouter.get('/:id', authRol(['user']), getOneUser);
 
-router.delete('/:id', authRol(['user']), deleteUser);
+export default userRouter;
 
-router.post('/', authRol(['user']), userValidator, handleValidationResults, createUser);
-
-router.put('/:id', authRol(['user']), updateUserValidator, handleValidationResults, updateUser);
-
-router.get('/:id', authRol(['user']), getOneUser);
-
-export default router;
