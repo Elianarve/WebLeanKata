725007850c554715e697458204ec99d045239f45
NuriaVicenteRodriguez <nuriavilla199@gmail.com>
2024-05-17 11:45:34 +0200

    merge feature-back

--- server/gemini.config.js
+++
@@ -1,5 +0,0 @@
-/**
- * Configuración de Gemini para el backend del proyecto.
- * Este archivo contiene la configuración específica para el servidor backend,
- * como la definición de rutas, middleware, etc.
- */
