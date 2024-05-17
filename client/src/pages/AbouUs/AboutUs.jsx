911a7e9e670941328b0ef91e6c8174598441f518
EstefanyBatPel <estefanybatpel@gmail.com>
2024-05-17 22:06:45 +0200

    add card of benefits

--- client/src/pages/AbouUs/AboutUs.jsx
+++ client/src/pages/AbouUs/AboutUs.jsx
@@ -12,9 +12,44 @@ const AboutUs = () => {
       <main>
         <section>
         <h3>¡Conoce nuestra <span>metodología</span> y mejora tus procesos!</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, numquam atque dolor provident saepe blanditiis laborum id incidunt veniam quam unde dolorum voluptas tenetur! Perspiciatis quibusdam eveniet facilis enim voluptate.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, numquam atque dolor provident saepe blanditiis laborum id incidunt veniam quam unde dolorum voluptas tenetur! Perspiciatis quibusdam eveniet facilis enim voluptate.</p>
+        <p>Originada en el Sistema de Producción de Toyota, se centra en la eliminación de desperdicios y la mejora continua 'Kaizen' para aumentar la eficiencia y la calidad. </p>
+        <p>Kata es un patrón o rutina de comportamiento que se practica regularmente hasta que se convierte en un hábito. </p>
         </section>
+
+
+  <div className='cardsContainer'>
+        <div className="card">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="card-inner">
+        <img src=""/>
+                <h4>Beneficio</h4>
+                <p>Descripcion</p>
+        </div>
+        </div>
+
+        <div className="card">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="card-inner">
+        <img src=""/>
+                <h4>Beneficio</h4>
+                <p>Descripcion</p>
+        </div>
+        </div>
+
+        <div className="card">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="card-inner">
+        <img src=""/>
+                <h4>Beneficio</h4>
+                <p>Descripcion</p>
+        </div>
+        </div>
+    </div>
+        <hr></hr>
+        
         <iframe width="560" height="315" src="https://www.youtube.com/embed/L8k0OJjKWSI?si=USM3f5VW1JU0WTxY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <p>Descripcion del video</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat illum numquam pariatur provident hic inventore asperiores ad, labore tenetur alias quasi! Recusandae reiciendis, nulla adipisci laboriosam quis nisi temporibus quae!Lorem Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto necessitatibus error ipsum tempora dolore! Perspiciatis quas iure consequuntur necessitatibus, quasi corporis nostrum ipsum error, perferendis ullam commodi molestias fuga. Fuga.</p>
