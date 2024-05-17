d6f97c2561457b1aef2856e4bed535b67cb4f2b0
EstefanyBatPel <estefanybatpel@gmail.com>
2024-05-17 19:53:09 +0200

    add AboutUs pagr

---
+++ client/src/pages/AbouUs/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AboutUs.css'
+
+const AboutUs = () => {
+  return (
+    <div className="container-title">
+          <h1 className='title-home'>Descubre la tabla de retos: <br /> ¡Tu camino hacia la mejora continua con LeanKata!</h1>
+          <button>Conoce mas sobre nuestra tecnología</button>
+        </div>
+  )
+}
+
+export default AboutUs
