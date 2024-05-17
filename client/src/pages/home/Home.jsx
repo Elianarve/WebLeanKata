a750b33ce329a706acfef04b020b028c35955f3b
EstefanyBatPel <estefanybatpel@gmail.com>
2024-05-17 18:33:28 +0200

    change the home table with lines

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -134,6 +134,7 @@ import "./Home.css";
 import "../../components/calendar/Calendar";
 import { getActualState } from '../../services/actualStateServices';
 import { io } from 'socket.io-client';
+import homeImg from '../../assets/img/Presentation.png'
 
 const Home = () => {
   const [challenges, setChallenges] = useState([]);
@@ -213,6 +214,7 @@ const Home = () => {
     <div className="home-container">
       <div className='container-principal'>
         <div className="container-title">
+          {/* <img src={homeImg}/> */}
           <h1 className='title-home'>Descubre la tabla de retos: <br /> ¡Tu camino hacia la mejora continua con LeanKata!</h1>
         </div>
         <div className="home-container-calendar">
