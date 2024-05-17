99d58292c3dbf9aee5aebc065aa4b4759effaa0b
EstefanyBatPel <estefanybatpel@gmail.com>
2024-05-17 21:18:08 +0200

    add responsive in aboutUs

--- client/src/pages/AbouUs/AboutUs.jsx
+++ client/src/pages/AbouUs/AboutUs.jsx
@@ -4,15 +4,21 @@ import './AboutUs.css'
 const AboutUs = () => {
   return (
     <div className='aboutUs-container'>
-      <section>
+      <header>
         <div className='leanKata-backdrop'>
           <h1>Lean<span>K</span>ata</h1>
         </div>
-      </section>
+      </header>
       <main>
-        <h3>Conoce nuestra metodología</h3>
-        <p>Descripcion de la metodología</p>
+        <section>
+        <h3>¡Conoce nuestra <span>metodología</span> y mejora tus procesos!</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, numquam atque dolor provident saepe blanditiis laborum id incidunt veniam quam unde dolorum voluptas tenetur! Perspiciatis quibusdam eveniet facilis enim voluptate.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, numquam atque dolor provident saepe blanditiis laborum id incidunt veniam quam unde dolorum voluptas tenetur! Perspiciatis quibusdam eveniet facilis enim voluptate.</p>
+        </section>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/L8k0OJjKWSI?si=USM3f5VW1JU0WTxY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>Descripcion del video</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat illum numquam pariatur provident hic inventore asperiores ad, labore tenetur alias quasi! Recusandae reiciendis, nulla adipisci laboriosam quis nisi temporibus quae!Lorem Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto necessitatibus error ipsum tempora dolore! Perspiciatis quas iure consequuntur necessitatibus, quasi corporis nostrum ipsum error, perferendis ullam commodi molestias fuga. Fuga.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptate sint aliquam non maxime porro a hic quos eum voluptas culpa, eos quis autem numquam dolor ducimus? At, est aut. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore aperiam aliquam, repellat, similique cum beatae quaerat iusto fugit sit quas totam. Veniam soluta in ipsum aut optio porro maiores ab.</p>
       </main>
     </div>
   )
