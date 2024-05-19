7827300cfac926626866c39c3a5ec390952a0cbb
Elianarve <elirve2@outlook.es>
2024-05-19 19:45:01 +0200

    changes-select-challenges-position

---
+++ client/src/components/ChallengeSelect/ChallengeSelect.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
+
+const ChallengeSelect = ({ challenges, selectedChallengeId, onSelectChange }) => {
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        const selectedChallengeId = event.target.value;
+        onSelectChange(selectedChallengeId);
+        navigate(`/home/card/${selectedChallengeId}`);
+    };
+
+    return (
+        <select value={selectedChallengeId} onChange={handleChange} className='container-select'>
+            {challenges.map((challenge) => (
+                <option key={challenge.id} value={challenge.id}>
+                    {challenge.name}
+                </option>
+            ))}
+        </select>
+    );
+};
+
+
+export default ChallengeSelect
