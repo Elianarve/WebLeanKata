2a8bf3f1b2f0f2b416f1605adbe3678035ce65b1
Elianarve <elirve2@outlook.es>
2024-05-20 11:12:24 +0200

    last-changes

--- server/database/connection_db.js
+++ server/database/connection_db.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
-import { DB_DEV_NAME, DB_USER, DB_PASSWORD, DB_TEST_NAME, NODE_ENV } from '../config.js';
+import { DB_DEV_NAME, DB_USER, DB_PASSWORD, DB_TEST_NAME, NODE_ENV, DB_HOST } from '../config.js';
 
 const DB_NAME = NODE_ENV === 'test' ? DB_TEST_NAME : DB_DEV_NAME;
 
 const connection_db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: 'localhost',
+  host: DB_HOST,
   dialect: 'mysql',
 
   logging: (msg) => {
